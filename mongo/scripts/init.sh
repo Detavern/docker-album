18d6aa3e64b0763c72003bd17f8a212aa1d3d196
Detavern <detavern@live.com>
2024-09-02 22:04:49 +0800

    mongo: update init

--- mongo/scripts/init.sh
+++ mongo/scripts/init.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+if which mongosh > /dev/null 2>&1; then
+  mongo_init_bin='mongosh'
+else
+  mongo_init_bin='mongo'
+fi
+
+"${mongo_init_bin}" << EOF
+use ${MONGO_INITDB_DATABASE}
 db.auth("${MONGO_INITDB_ROOT_USERNAME}", "${MONGO_INITDB_ROOT_PASSWORD}")
 db.createUser({
   user: "${INIT_SCRIPT_USERNAME}",
@@ -6,3 +16,4 @@ db.createUser({
     { db: "${INIT_SCRIPT_DATABASE}", role: "dbOwner" },
   ]
 })
+EOF
