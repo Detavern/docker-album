a1a37f7bbf5cad55e2601f563d05813ff17c4cb2
Detavern <detavern@live.com>
2025-04-19 01:14:15 +0800

    docker.easytier-bird: fix entrypoint

--- docker/easytier-bird/entrypoint.sh
+++ docker/easytier-bird/entrypoint.sh
@@ -6,13 +6,14 @@ set -eu
 # first run
 FIRST_RUN="/root/.firstrun"
 BIRD_CONF_DIR="/etc/bird"
+BIRD_CONF_FILE="${BIRD_CONF_DIR}/bird.conf"
 if [ ! -f "${FIRST_RUN}" ]; then
     # init bird
-    if [ ! -f "${BIRD_CONF_PATH}" ]; then
-        cp /root/bird.tmpl.conf ${BIRD_CONF_DIR}/bird.conf
+    if [ ! -f "${BIRD_CONF_FILE}" ]; then
+        cp /root/bird.tmpl.conf ${BIRD_CONF_FILE}
 
         # router id
-        sed -i "s/^\(router id\) .*\?;/\1 ${BIRD_ROUTER_ID};/" ${BIRD_CONF_DIR}/bird.conf
+        sed -i "s/^\(router id\) .*\?;/\1 ${BIRD_ROUTER_ID};/" ${BIRD_CONF_FILE}
 
         # prepare ospf directory
         BIRD_CONF_OSPF_DIR="${BIRD_CONF_DIR}/ospf"
