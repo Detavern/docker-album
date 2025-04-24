88a3413baf531e83c2943ccdba08c707917456de
Detavern <detavern@live.com>
2025-04-25 06:58:46 +0800

    docker.easytier-bird: add birdh

--- docker/easytier-bird/entrypoint.sh
+++ docker/easytier-bird/entrypoint.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-set -eu
-
+set -euo pipefail
 
 # first run
 FIRST_RUN="/root/.firstrun"
 BIRD_CONF_DIR="/etc/bird"
 BIRD_CONF_FILE="${BIRD_CONF_DIR}/bird.conf"
+
+comment_block_after_header() {
+  local header="$1"
+  local file="$2"
+
+  sed -i "/^$header/{:a; n; /^$/b; s/^/## /; ba}" "$file"
+}
+
 if [ ! -f "${FIRST_RUN}" ]; then
+    # check env
+    if [ -z "${EASYTIER_CONFIG_SERVER}" ]; then
+        echo "EASYTIER_CONFIG_SERVER not set, check it manually" 2>&1
+        exit 1
+    fi
+
     # init bird
     if [ ! -f "${BIRD_CONF_FILE}" ]; then
         cp /root/bird.tmpl.conf ${BIRD_CONF_FILE}
@@ -15,16 +28,38 @@ if [ ! -f "${FIRST_RUN}" ]; then
         # router id
         sed -i "s/^\(router id\) .*\?;/\1 ${BIRD_ROUTER_ID};/" ${BIRD_CONF_FILE}
 
-        # prepare ospf directory
-        BIRD_CONF_OSPF_DIR="${BIRD_CONF_DIR}/ospf"
-        mkdir -p ${BIRD_CONF_OSPF_DIR}
+        # static ipv4
+        if [ "${BIRD_STATIC_IPV6_ENABLED}" = "0" ]; then
+            comment_block_after_header "### static ipv4" ${BIRD_CONF_FILE}
+        fi
 
-        # prepare ospf static
+        # static ipv6
+        if [ "${BIRD_STATIC_IPV4_ENABLED}" = "0" ]; then
+            comment_block_after_header "### static ipv6" ${BIRD_CONF_FILE}
+        fi
+
+        # ospf v2
+        if [ "${BIRD_OSPF_V2_ENABLED}" = "0" ]; then
+            comment_block_after_header "### ospf v2" ${BIRD_CONF_FILE}
+        fi
+
+        # ospf v3
+        if [ "${BIRD_OSPF_V3_ENABLED}" = "0" ]; then
+            comment_block_after_header "### ospf v3" ${BIRD_CONF_FILE}
+        fi
+
+        # prepare static directory
+        BIRD_CONF_STATIC4_DIR="${BIRD_CONF_DIR}/static4"
+        mkdir -p ${BIRD_CONF_STATIC_DIR}
+        BIRD_CONF_STATIC6_DIR="${BIRD_CONF_DIR}/static6"
+        mkdir -p ${BIRD_CONF_STATIC_DIR}
     fi
+
+    # add cron
+    echo "0 17 * * *" >> /etc/crontabs/root
 
     # write
     touch ${FIRST_RUN}
 fi
-
 
 exec "$@"
