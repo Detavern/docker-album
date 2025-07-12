7e2a995469d8c6b12cc755dadfbb45445983ff96
Detavern <Detavern@users.noreply.github.com>
2025-07-13 04:50:36 +0800

    update

--- filebrowser/init.sh
+++ filebrowser/init.sh
@@ -4,7 +4,7 @@
 docker run --rm -v /data/filebrowser:/tmp filebrowser/filebrowser \
     -d /tmp/filebrowser.db config init
 docker run --rm -v /data/filebrowser:/tmp filebrowser/filebrowser \
-    -d /tmp/filebrowser.db config set -a 0.0.0.0 -p 80 -b $FILE_BROWSER_SERVER_DOMAIN \
-    --recaptcha.key $RECAPTCHA_KEY --recaptcha.secret $RECAPTCHA_SECRET
+    -d /tmp/filebrowser.db config set -a 0.0.0.0 -p 80 -b ${FILE_BROWSER_SERVER_DOMAIN:?} \
+    --recaptcha.key ${RECAPTCHA_KEY:?} --recaptcha.secret ${RECAPTCHA_SECRET:?}
 docker run --rm -v /data/filebrowser:/tmp filebrowser/filebrowser \
-    -d /tmp/filebrowser.db users add $FILE_BROWSER_ADMIN_USER $FILE_BROWSER_ADMIN_PASSWORD --perm.admin
+    -d /tmp/filebrowser.db users add ${FILE_BROWSER_ADMIN_USER:?} ${FILE_BROWSER_ADMIN_PASSWORD:?} --perm.admin
